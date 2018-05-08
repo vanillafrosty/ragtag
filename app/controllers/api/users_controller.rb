2481cd234aec0092679f96b2cf0168f466781a62
vanillafrosty <dwu.inq@gmail.com>
2018-05-08 14:22:13 -0400

    add DOM event listener, create React hook onto root div, create session API utils

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
 
 
---
+++ frontend/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>THIS IS NOT A DANCE</h1>
+      </div>
+    );
+  }
+}
