089c0c40a3cf84cd574f6537f1ae69445e91833f
vanillafrosty <dwu.inq@gmail.com>
2018-05-08 14:04:28 -0400

    create sessions controller for auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Api::SessionsController < ApplicationController
+
+
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      log_in(@user)
+      render '/api/users/show'
+    else
+      render json: ['Invalid Credentials'], status: 401
+    end
+  end
+
+
+
+  def destroy
+    if current_user
+      sign_out
+      render json: {}
+    else
+      render json: ['No current user!'], status: 404
+    end
+  end
+
+
+
+end
