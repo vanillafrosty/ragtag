285d5d774676850d6286fd95903cb836dfc56e29
vanillafrosty <dwu.inq@gmail.com>
2018-05-08 12:42:47 -0400

    migrate users and posts

--- db/migrate/20180508161712_create_posts.rb
+++ db/migrate/20180508161712_create_posts.rb
@@ -1,6 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
+      t.text :body
+      t.integer :user_id, null: false
+      t.string :img_url, null: false
+
+      t.timestamps
     end
+
+    add_index :posts, :user_id
   end
 end
