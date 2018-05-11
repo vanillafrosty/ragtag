b3ea29d43f08aec2a92ebf249605e4b2c20f2b49
vanillafrosty <dwu.inq@gmail.com>
2018-05-11 17:36:39 -0400

    add FileReader functionality for image previews, update state in new post container, add A LOT of STYLING

--- frontend/components/post_new_container.jsx
+++ frontend/components/post_new_container.jsx
@@ -5,8 +5,11 @@ export default class PostNewContainer extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      body: ''
+      body: '',
+      imgFile: null,
+      imgUrl: null
     };
+    this.updateFile = this.updateFile.bind(this);
   }
 
   handleChange(field) {
@@ -18,17 +21,40 @@ export default class PostNewContainer extends React.Component {
     };
   }
 
+  fileBind(file, result) {
+    return (e) => {
+      e.preventDefault();
+      this.setState({ imgFile: file, imgUrl: result});
+    };
+  }
+
+  updateFile(e) {
+    e.preventDefault();
+    let file = e.currentTarget.files[0];
+    var fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      this.setState({ imgFile: file, imgUrl: fileReader.result });
+    };
+    if (file) {
+      fileReader.readAsDataURL(file);
+    } else {
+      this.setState({ imgUrl: "", imgFile: null });
+    }
+  }
+
   render() {
     return (
       <div className="post-new-container">
         <form>
           <div className="post-new-image">
+            <img className="post-new-image-preview" src={this.state.imgUrl} />
           </div>
 
           <div className="post-new-side">
-            <div className="x-button" onClick={this.props.closeModal}><i class="fas fa-times fa-lg"></i></div>
+            <div className="x-button" onClick={this.props.closeModal}><i className="fas fa-times fa-lg"></i></div>
             <div className="post-new-submit">
-              <textarea rows="4" cols="30" maxlength="280" placeholder="Caption" value={this.state.body} onChange={this.handleChange('body')}>
+              <input type="file" onChange={this.updateFile} />
+              <textarea rows="4" cols="30" maxLength="280" placeholder="Caption" value={this.state.body} onChange={this.handleChange('body')}>
               </textarea>
 
               <button type="button">Share</button>
