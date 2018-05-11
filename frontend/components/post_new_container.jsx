05744911e9b46b6e77e0efe91dae168a3e976b62
vanillafrosty <dwu.inq@gmail.com>
2018-05-11 16:36:53 -0400

    change demo prof pic, continue styling on new post container

--- frontend/components/post_new_container.jsx
+++ frontend/components/post_new_container.jsx
@@ -21,14 +21,19 @@ export default class PostNewContainer extends React.Component {
   render() {
     return (
       <div className="post-new-container">
-        <div className="x-button" onClick={this.props.closeModal}>&times;</div>
         <form>
-          <label>Caption
-            <input type="text" value={this.state.body} onChange={this.handleChange('body')} />
-          </label>
+          <div className="post-new-image">
+          </div>
 
-          <button type="button">Share</button>
+          <div className="post-new-side">
+            <div className="x-button" onClick={this.props.closeModal}><i class="fas fa-times fa-lg"></i></div>
+            <div className="post-new-submit">
+              <textarea rows="4" cols="30" maxlength="280" placeholder="Caption" value={this.state.body} onChange={this.handleChange('body')}>
+              </textarea>
 
+              <button type="button">Share</button>
+            </div>
+          </div>
         </form>
       </div>
     );
