8b9eec21bc0c12bde45894bfa0e4190f9ec7a3b8
vanillafrosty <dwu.inq@gmail.com>
2018-05-16 11:05:35 -0400

    change profileInfo to a class component, add state for editing bio

--- frontend/components/profile/profile_info.jsx
+++ frontend/components/profile/profile_info.jsx
@@ -1,34 +1,51 @@
 import React from 'react';
 
 
-const ProfileInfo = (props) => {
+export default class ProfileInfo extends React.Component {
 
-  const followPrompt = props.followed ? 'Unfollow' : 'Follow';
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false
+    };
+    this.handleEdit = this.handleEdit.bind(this);
+  }
 
-  return (
-    <div className="profile-info-container">
-      <div className="profile-pic">
-        <img src={props.user.avatar_url} />
+
+  handleEdit(e) {
+    e.preventDefault();
+    this.setState({
+      editing: !this.state.editing
+    });
+  }
+
+  render() {
+    const followPrompt = this.props.followed ? 'Unfollow' : 'Follow';
+    console.log(this.state.editing);
+    return (
+      <div className="profile-info-container">
+        <div className="profile-pic">
+          <img src={this.props.user.avatar_url} />
+        </div>
+        <ul className="profile-info">
+          <li className="profile-info-first">
+            <div className="user-text">{this.props.user.username}</div>
+            {this.props.user.id === this.props.currentUser ? (<button type="button" onClick={this.handleEdit}>Edit Bio</button>) :
+              (<button type="button" onClick={this.props.createFollow}>{followPrompt}</button>)}
+            {this.props.user.id === this.props.currentUser ? <button type="button" onClick={this.props.openCreateModal}>New Post</button> : ''}
+            {this.props.user.id === this.props.currentUser ? <button type="button" onClick={this.props.logout}>Logout</button> : ''}
+          </li>
+          <li className="profile-info-second">
+            <div className="profile-text">{`${this.props.posts.length} posts`}</div>
+            <div className="profile-text">{`${this.props.user.follows.length} followers`}</div>
+          </li>
+          <li className="profile-info-third">
+            BIO HERE
+          </li>
+        </ul>
+
       </div>
-      <ul className="profile-info">
-        <li className="profile-info-first">
-          <div className="user-text">{props.user.username}</div>
-          {props.user.id === props.currentUser ? (<button type="button">Edit Bio</button>) :
-            (<button type="button" onClick={props.createFollow}>{followPrompt}</button>)}
-          {props.user.id === props.currentUser ? <button type="button" onClick={props.openCreateModal}>New Post</button> : ''}
-          {props.user.id === props.currentUser ? <button type="button" onClick={props.logout}>Logout</button> : ''}
-        </li>
-        <li className="profile-info-second">
-          <div className="profile-text">{`${props.posts.length} posts`}</div>
-          <div className="profile-text">{`${props.user.follows.length} followers`}</div>
-        </li>
-        <li className="profile-info-third">
-          BIO HERE
-        </li>
-      </ul>
+    );
+  }
 
-    </div>
-  );
-};
-
-export default ProfileInfo;
+}
