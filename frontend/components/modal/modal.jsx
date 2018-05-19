4e16bc1633bc921656c2eee56804b92987e08f82
vanillafrosty <dwu.inq@gmail.com>
2018-05-19 12:41:02 -0400

    refactor modal component to give it more breathing room

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
 import { closeModal } from '../../actions/modal_actions';
 import { connect } from 'react-redux';
 import PostNewContainer from './post_new_container';
 import PostShowContainer from './post_show_container';
 import PostExploreContainer from './post_explore_container';
-import { createPost, clearErrors, updatePost, deletePost } from '../../actions/post_actions';
-import { createLike } from '../../actions/like_actions';
-import { fetchCommentsAndUsers, clearComments, createComment } from '../../actions/comment_actions';
 
-const Modal = ({users, errors, clearErrors, modal, closeModal, createPost, post, updatePost, deletePost, createLike, liked, fetchCommentsAndUsers, comments, clearComments, sessionUser, currentUser, createComment}) => {
+const Modal = ({modal, closeModal}) => {
 
   if (!modal.status) {
     return null;
@@ -39,39 +35,15 @@ const Modal = ({users, errors, clearErrors, modal, closeModal, createPost, post,
 }
 
 const mapStateToProps = (state, ownProps) => {
-  let modal, post, users, liked, sessionUser, currentUser;
-  modal = state.ui.modal;
-  sessionUser = state.entities.users[state.session.id];
-  let matched = ownProps.location.pathname.match(/\d+/);
-  if (matched) {
-    currentUser = state.entities.users[matched[0]];
-  }
-  //set dummy currentUser for when component renders for the first time,
-  //before fetchCommentsAndUsers fires
-  if (currentUser === undefined) {
-    currentUser = {};
-  }
   return {
-    sessionUser: sessionUser,
-    currentUser: currentUser,
-    modal: modal,
-    post: post,
-    users: users,
-    liked: liked,
-    comments: state.entities.comments
+    modal: state.ui.modal
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    closeModal: () => dispatch(closeModal()),
-    updatePost: (post) => dispatch(updatePost(post)),
-    deletePost: (id) => { return dispatch(deletePost(id)) },
-    createLike: (id) => dispatch(createLike(id)),
-    fetchCommentsAndUsers: (id) => dispatch(fetchCommentsAndUsers(id)),
-    clearComments: () => dispatch(clearComments()),
-    createComment: (comment) => dispatch(createComment(comment))
+    closeModal: () => dispatch(closeModal())
   };
 };
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Modal));
+export default connect(mapStateToProps, mapDispatchToProps)(Modal);
